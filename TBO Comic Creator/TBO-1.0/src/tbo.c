51dc71ceb25647492ea45bfb26d5ffffd9dd0895
wachin <wachin.id@gmail.com>
2020-06-08 13:54:51 -0500

    Guardar automáticamente el commit P3 lun jun  8 13:54:51 -05 2020

---
+++ TBO Comic Creator/TBO-1.0/src/tbo.c
@@ -0,0 +1,50 @@
+/*
+ * This file is part of TBO, a gnome comic editor
+ * Copyright (C) 2010  Daniel Garcia Moreno <dani@danigm.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <gtk/gtk.h>
+#include <glib/gi18n.h>
+
+#include "config.h"
+
+#include "custom-stock.h"
+#include "tbo-window.h"
+#include "comic.h"
+
+int main (int argc, char**argv){
+
+#ifdef ENABLE_NLS
+    /* Initialize the i18n stuff */
+    bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
+#endif
+
+    TboWindow *tbo;
+
+    gtk_init (&argc, &argv);
+    load_custom_stock ();
+    tbo = tbo_new_tbo (800, 450);
+    if (argc == 2)
+        tbo_comic_open (tbo, argv[1]);
+
+    gtk_main ();
+
+    return 0;
+}
+
