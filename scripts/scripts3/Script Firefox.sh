75059a626cd95f556f9ea37ae5e5f33906d6e89a
wachin <wachin.id@gmail.com>
2020-11-25 16:00:58 -0500

    Guardar automáticamente el commit P1 mié nov 25 16:00:34 -05 2020

---
+++ scripts/scripts3/Script Firefox.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+rm -r ~/.mozilla
+tar -xf ~/.mozilla.tar.gz -C ~/
+firefox
+
+
+## Primero sacar respaldo de la carpeta de Mozilla
+## yo le he copiado a /home/washington/Firefox-ok/.mozilla 
+## y lo he comprimido allí para tener ese respaldo allí
+## y luego el comprimido .mozilla.tar.gz lo he puesto en HOME
+## para ahora si desde allí lanzar este
+
